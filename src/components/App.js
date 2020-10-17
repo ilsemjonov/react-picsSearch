3c0a12ae26c6b2668e1d01957f379dd621abbb53
Ilja Semjonov <ilsemjonov@gmail.com>
2020-10-17 17:39:28 +0300

    making axios request

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends Component {
-    onSearchSubmit(term) {
-        console.log(term)
+    async onSearchSubmit(term) {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: { Authorization: 'Client-ID xVxdNsjpiXiGbxO7cU62o7PV1OaqmqEO4wQw5-oxFX0' }
+        });
+        console.log(response.data.results)
     }
 
     render() {
